e23372c67e928411de88c22469fddd884896469b
Xavier Davis <xdavis1@icloud.com>
2019-10-03 00:02:45 -0700

    just getting started

---
+++ Liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+ 
+
+
+var spotify = new Spotify({
+  id: 'c376e1ce7af04f86905c76a311e9d0ba' ,
+  secret : 'ae8590f02b994aec84dbc3134237d716' 
+});
+ 
+
