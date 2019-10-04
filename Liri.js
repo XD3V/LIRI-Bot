7928e0e98c6ef18f7230f60ff52cef75662e24fb
Xavier Davis <xdavis1@icloud.com>
2019-10-04 10:13:39 -0700

    allows user to choose music or a movie

--- Liri.js
+++ Liri.js
@@ -1,26 +1,60 @@
 // Dependency for dotenv npm package
 require("dotenv").config();
 
-var keys = require("./keys.js");
+let keys = require("./keys.js");
 // Dependency for node-spotify-api npm package
-var Spotify = require('node-spotify-api');
- 
+let Spotify = require("node-spotify-api");
+
 // Dependency for inquirer npm package
-var inquirer = require("inquirer");
+let inquirer = require("inquirer");
 
 // Constructor function for creating LIRI objects
-function Liri(concert,song,movie){
-this.concert;
-this.song;
-this.movie;
+function Liri(concert, artist, song, movie) {
+  this.concert = concert;
+  this.song = song;
+  this.movie = movie;
+  this.artist = artist;
 
-
-this.bandsInTown = function (){
-    let artist = process.argv[2];
-
-let band = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+  // creating a new method called BandsInTown.This intels if the user wants to either listen to somone or not.
+  this.bandsInTown = function() {
+    // this is stating if the user wants to input a artist then it goes through the code.
+    if (this.artist === true)
+      console.log("/nHere is the artist you were looking for ");
+    let band =
+      "https://rest.bandsintown.com/artists/" +
+      artist +
+      "/events?app_id=codingbootcamp";
+  };
 }
 
-
+function mainMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to choose a Music or a Movie",
+        choices: ["Music", "Movie"],
+        name: "list"
+      },
+      {
+        type: "confirm",
+        message: "Are you sure:",
+        name: "confirm",
+        default: true
+      }
+    ])
+    .then(function(inquirerResponse) {
+      // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+      if (inquirerResponse.confirm === true) {
+        console.log("\nWelcome " + inquirerResponse.Music);
+        console.log(
+          "\nYour " + inquirerResponse.Music + " is ready for battle!\n"
+        );
+      }
+      if (inquirerResponse.confirm === false) {
+          console.log("\n")
+        mainMenu();
+      }
+    });
 }
-bandsInTown();
+mainMenu();
